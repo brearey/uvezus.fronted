503c15a95513b493a8f39b35bd16963742da79c5
Lorriant <brearey4@gmail.com>
2025-09-12 20:40:15 +0900

    deleted angular

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import {SplashScreenComponent} from "./screens/splash-screen/splash-screen.component";
-import {EmailScreenComponent} from "./screens/email-screen/email-screen.component";
-
-export const routes: Routes = [
-  {path: '', redirectTo: 'splash', pathMatch: 'full'},
-  {path: 'splash', component: SplashScreenComponent},
-  {path: 'email', component: EmailScreenComponent},
-];
