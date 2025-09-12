7416d666e7e52de449f62fc3219721dc2dd2812c
Lorriant <brearey4@gmail.com>
2025-09-12 20:56:52 +0900

    format with 4 spaces for tab

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
+import { SplashScreen } from './pages/splash-screen'
 
 function App() {
-  return (
-    <>
-      <h1>Hello wow</h1>
-    </>
-  )
+    return (
+        <>
+            <SplashScreen />
+        </>
+    )
 }
 
 export default App
---
+++ src/pages/splash-screen/index.tsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+function SplashScreen() {
+    return (
+        <>
+            <h1>SplashScreen</h1>
+        </>
+    )
+}
+
+export { SplashScreen }
