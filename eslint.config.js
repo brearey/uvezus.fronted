8a2fef8c7c6d5f360e821d8adc22966bb771bbf4
Lorrain Tapioca <brearey88@yandex.ru>
2025-09-14 19:34:51 +0900

    installed tanstack query

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import pluginQuery from '@tanstack/eslint-plugin-query'
 
 export default tseslint.config([
 	globalIgnores(['dist']),
@@ -20,4 +21,5 @@ export default tseslint.config([
 			globals: globals.browser,
 		},
 	},
+	...pluginQuery.configs['flat/recommended'],
 ])
