c26921963973cb8f87cc9dc415cf29711468bd66
lorriant <brearey88@yandex.ru>
2025-09-16 18:07:28 +0900

    isntalled modal but don't work

---
+++ src/components/address-modal/address-modal.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import Modal from 'react-modal'
+
+import './address-modal-module.css'
+
+Modal.setAppElement('#root');
+
+function AddressModal() {
+  const [modalIsOpen, setIsOpen] = useState(true);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  return (
+    <div>
+      <button onClick={openModal}>Open Modal</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        className='modal'
+        contentLabel="Example Modal"
+      >
+        <button onClick={closeModal}>close</button>
+        <div>I am a modal</div>
+        <form>
+          <input />
+          <button>tab navigation</button>
+          <button>stays</button>
+          <button>inside</button>
+          <button>the modal</button>
+        </form>
+      </Modal>
+    </div>
+  );
+}
+
+export { AddressModal }
