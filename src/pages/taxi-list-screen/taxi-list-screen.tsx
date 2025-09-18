e09c94c4341ec7ee837421106e21094caa9c9cf0
Lorrain Tapioca <brearey88@yandex.ru>
2025-09-18 22:07:07 +0900

    Custom modal changed to native prompt() function. Added modal for adult and child count

--- src/pages/taxi-list-screen/taxi-list-screen.tsx
+++ src/pages/taxi-list-screen/taxi-list-screen.tsx
@@ -1,29 +1,38 @@
 import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser, faLocationDot } from '@fortawesome/free-solid-svg-icons'
-import { AddressModal } from '../../components/address-modal/address-modal'
 import uvezusLogo from '../../assets/logo.svg'
 import './taxi-list-module.css'
+import { toast } from 'react-toastify'
+
+const isCountValid = (count: number) => count && count > 0 && count < 8
 
 export function TaxiListScreen() {
 	const [adultCount, setAdultCount] = useState(1)
 	const [childCount, setChildCount] = useState(0)
 	const [address, setAddress] = useState('с. Чапаево, ул. Николаева 27/2')
-	const [isAddressModalOpen, setIsAddressModalOpen] = useState(false)
-	const [tempAddress, setTempAddress] = useState(address)
 
-	const handleSaveAddress = () => {
-		setAddress(tempAddress)
-		setIsAddressModalOpen(false)
+	const openAddressModal = () => {
+		const temp = prompt('Введите ваш адрес') || address
+		setAddress(temp)
 	}
 
-	const handleOpenModal = () => {
-		setTempAddress(address)
-		setIsAddressModalOpen(true)
+	const openAdultModal = () => {
+		const temp: number | null = Number(prompt('Введите количество взрослых пассажиров'))
+		if (isCountValid(temp)) {
+			setAdultCount(temp)
+		} else {
+			toast.warn('Введите корректное количество')
+		}
 	}
 
-	const handleCloseModal = () => {
-		setIsAddressModalOpen(false)
+	const openChildModal = () => {
+		const temp: number | null = Number(prompt('Введите количество пассажиров-детей'))
+		if (isCountValid(temp)) {
+			setChildCount(temp)
+		} else {
+			toast.warn('Введите корректное количество')
+		}
 	}
 
 	return (
@@ -38,16 +47,16 @@ export function TaxiListScreen() {
 			{/* passenger and address */}
 			<div className="passengers-wrapper">
 				<div className="left">
-					<div className="passengers">
+					<div className="passengers" onClick={openAdultModal}>
 						<FontAwesomeIcon icon={faUser} className="icon adult-icon" />
 						<span>{adultCount} взрос.</span>
 					</div>
-					<div className="passengers">
+					<div className="passengers" onClick={openChildModal}>
 						<FontAwesomeIcon icon={faUser} className="icon child-icon" />
 						<span>{childCount} реб.</span>
 					</div>
 				</div>
-				<div className="right" onClick={handleOpenModal}>
+				<div className="right" onClick={openAddressModal}>
 					<FontAwesomeIcon
 						icon={faLocationDot}
 						className="icon location-icon"
@@ -56,13 +65,6 @@ export function TaxiListScreen() {
 				</div>
 			</div>
 
-			<AddressModal
-				isAddressModalOpen={isAddressModalOpen}
-				handleCloseModal={handleCloseModal}
-				tempAddress={tempAddress}
-				setTempAddress={setTempAddress}
-				handleSaveAddress={handleSaveAddress}
-			/>
 		</div>
 	)
 }
