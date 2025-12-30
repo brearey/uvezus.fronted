36855fb2e5348f851490e85cecab2de30932c3c4
lorriant <brearey4@gmail.com>
2025-12-30 16:08:34 +0900

    temp hardcode server url

--- src/http/api.ts
+++ src/http/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { logger } from '../util/logger'
 
-const serverUrl = import.meta.env.VITE_SERVER_URL || 'http://localhost:3002'
+const serverUrl = import.meta.env.VITE_SERVER_URL || 'https://lorriant.ru'
 logger.info(`serverUrl = ${serverUrl}`)
 const baseURL = serverUrl + '/api'
 
