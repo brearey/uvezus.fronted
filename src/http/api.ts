0f196a90b4fd523e3df888a6ef5cc5728c404181
lorriant <brearey4@gmail.com>
2025-12-30 16:04:06 +0900

    log server url

--- src/http/api.ts
+++ src/http/api.ts
@@ -1,6 +1,8 @@
 import axios from 'axios'
+import { logger } from '../util/logger'
 
 const serverUrl = import.meta.env.VITE_SERVER_URL || 'http://localhost:3002'
+logger.info(`serverUrl = ${serverUrl}`)
 const baseURL = serverUrl + '/api'
 
 export const api = axios.create({
---
+++ src/util/logger.ts
@@ -0,0 +1,10 @@
+export const logger = {
+	now: new Date().toLocaleString(),
+	instance: console, // тут можно поставить логгер получше типа winston, pino
+	info: (message: string) => {
+		logger.instance.info(`${logger.now} | INFO | ${message}`)
+	},
+	error: (error: Error) => {
+		logger.instance.error(`${logger.now} | ERROR | ${error.name} ${error.message}`)
+	},
+}
