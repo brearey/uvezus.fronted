35fde23d1245e7007125ba3e69e51537c1baa93a
Lorriant <brearey4@gmail.com>
2026-01-01 15:52:28 +0900

    deleted log server url

--- src/http/api.ts
+++ src/http/api.ts
@@ -1,8 +1,6 @@
 import axios from 'axios'
-import { logger } from '../util/logger'
 
 const serverUrl = import.meta.env.VITE_SERVER_URL || 'http://localhost:3002'
-logger.info(`serverUrl = ${serverUrl}`)
 const baseURL = serverUrl + '/uvezus'
 
 export const api = axios.create({
