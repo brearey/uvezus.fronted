0addc3ec6f5a208795d57c6b262dbd4db800b442
lorriant <brearey4@gmail.com>
2026-01-01 13:43:38 +0900

    path /api to /uvezus

--- src/http/api.ts
+++ src/http/api.ts
@@ -3,7 +3,7 @@ import { logger } from '../util/logger'
 
 const serverUrl = import.meta.env.VITE_SERVER_URL || 'http://localhost:3002'
 logger.info(`serverUrl = ${serverUrl}`)
-const baseURL = serverUrl + '/api'
+const baseURL = serverUrl + '/uvezus'
 
 export const api = axios.create({
 	baseURL: baseURL,
